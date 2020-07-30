8e35dd1db03c2f81a6ebc1c7b1a69a46e48902cd
SandeSydney <sandesydney01@gmail.com>
2020-07-30 16:13:47 +0300

    corrected wrong spelling

--- lib/services/db_firestore_api.dart
+++ lib/services/db_firestore_api.dart
@@ -5,7 +5,7 @@ abstract class DbApi {
   Stream<List<Journal>> getJournalList(String uid);
   Future<Journal> getJournal(String documentID);
   Future<bool> addJournal(Journal journal);
-  void updateJourna(Journal journal);
+  void updateJournal(Journal journal);
   void updateJournalWithTransaction(Journal journal);
   void deleteJournal(Journal journal);
 }
