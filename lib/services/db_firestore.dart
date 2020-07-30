9a40b84f4d7ccad285b80c9310fbf7b50a871b9f
SandeSydney <sandesydney01@gmail.com>
2020-07-30 16:14:48 +0300

    class with methods to call onto the firestore API

---
+++ lib/services/db_firestore.dart
@@ -0,0 +1,62 @@
+import 'dart:html';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:journal/models/journal.dart';
+import 'db_firestore_api.dart';
+
+class DbFirestoreService implements DbApi {
+  final Firestore _firestore = Firestore.instance;
+  final String _collectionJournals = 'journals';
+
+  DbFirestoreService() {
+    _firestore.settings(timestampsInSnapshotsEnabled: true);
+  }
+
+  // retrieve journal entries
+  Stream<List<Journal>> getJournalList(String uid) {
+    return _firestore
+        .collection(_collectionJournals)
+        .where('uid', isEqualTo: uid)
+        .snapshots()
+        .map((QuerySnapshot snapshot) {
+      List<Journal> _journalDocs =
+          snapshot.documents.map((doc) => Journal.fromDoc(doc)).toList();
+      // sort in descending order
+      _journalDocs.sort((comp1, comp2) => comp2.date.compareTo(comp1.date));
+      return _journalDocs;
+    });
+  }
+
+  // Adding a new entry
+  Future<bool> addJournal(Journal journal) async {
+    DocumentReference _documentReference =
+        await _firestore.collection(_collectionJournals).add({
+      'date': journal.date,
+      'mood': journal.mood,
+      'note': journal.note,
+      'uid': journal.uid,
+    });
+    return _documentReference.documentID != null;
+  }
+
+  // Update the journal entry
+  void updateJournal(Journal journal) async {
+    await _firestore
+        .collection(_collectionJournals)
+        .document(journal.documentID)
+        .updateData({
+      'date': journal.date,
+      'mood': journal.mood,
+      'note': journal.note,
+    }).catchError((error) => print('Error updating: $error'));
+  }
+
+  // deleting a journal entry
+  void deleteJournal(Journal journal) async {
+    await _firestore
+        .collection(_collectionJournals)
+        .document(journal.documentID)
+        .delete()
+        .catchError((error) => print('Error deleting: $error'));
+  }
+}
