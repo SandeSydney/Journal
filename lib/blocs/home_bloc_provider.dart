e12a36053dfa29882c3ba427b19f7e8729e174cf
SandeSydney <sandesydney01@gmail.com>
2020-08-03 08:51:58 +0300

    Bloc provider for the homeBloc

---
+++ lib/blocs/home_bloc_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'home_bloc.dart';
+
+class HomeBlocProvider extends InheritedWidget {
+  // class variables
+  final HomeBloc homeBloc;
+  final String uid;
+
+  // constructor
+  const HomeBlocProvider(Key key, Widget child, this.homeBloc, this.uid)
+      : super(key: key, child: child);
+
+  static HomeBlocProvider of(BuildContext context) {
+    return (context.dependOnInheritedWidgetOfExactType() as HomeBlocProvider);
+  }
+
+  // check whether the homeBloc does not equal to the old HomeBlocProvider
+  @override
+  bool updateShouldNotify(HomeBlocProvider old) => homeBloc != old.homeBloc;
+}
